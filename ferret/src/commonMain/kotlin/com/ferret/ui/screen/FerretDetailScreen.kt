09a8f0cee5b2bcb1db76dc86c4c5ebe41c1be166
Aditya-gupta99 <ashishgupta9936ad@gmail.com>
2026-07-20 00:50:21 +0530

    fix: fix comments

--- ferret/src/commonMain/kotlin/com/ferret/ui/screen/FerretDetailScreen.kt
+++ ferret/src/commonMain/kotlin/com/ferret/ui/screen/FerretDetailScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -84,7 +85,7 @@ fun FerretDetailScreenContent(
 
     if (network == null) return
 
-    var showShareDialog by remember { mutableStateOf(false) }
+    var showShareDialog by rememberSaveable { mutableStateOf(false) }
 
     val tabs: List<FerretNetworkDetailTab> =
         if (network.isWebSocket) {
